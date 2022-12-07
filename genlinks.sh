10b30d2cb45fe9b8aebcb6f050c820d79ed733df
Tejas Luthra <45141187+tejas-luthra@users.noreply.github.com>
2022-12-06 21:18:47 -0700

    Added a script to link files

---
+++ genlinks.sh
@@ -0,0 +1,22 @@
+#Creates links between repo and config files through black magic
+#
+#Again make sure all dotfiles are in /home/$USER/Github/hyprland-dotfiles/ or else this will not work
+#
+
+#links .zshrc
+ln -sf /home/$USER/Github/hyprland-dotfiles/.zshrc /home/$USER/.
+
+#links the config file stuff
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/dconf /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/dunst /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/hypr /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/neofetch /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/nvim /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/rofi /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/scripts /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/thefuck /home/$USER/.config/.
+
+#links pictures and other stuff
+ln -sf /home/$USER/Github/hyprland-dotfiles/Pictures /home/$USER/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/pkglist_aur.txt /home/$USER/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/pkglist.txt /home/$USER/.
