476242db484307bb05b6693fcb557ae3626233e9
Tejas Luthra <45141187+tejas-luthra@users.noreply.github.com>
2022-12-06 22:36:16 -0700

    Fixed stuff

--- genlinks.sh
+++ genlinks.sh
@@ -15,6 +15,7 @@ ln -sf /home/$USER/Github/hyprland-dotfiles/config/nvim /home/$USER/.config/.
 ln -sf /home/$USER/Github/hyprland-dotfiles/config/rofi /home/$USER/.config/.
 ln -sf /home/$USER/Github/hyprland-dotfiles/config/scripts /home/$USER/.config/.
 ln -sf /home/$USER/Github/hyprland-dotfiles/config/thefuck /home/$USER/.config/.
+ln -sf /home/$USER/Github/hyprland-dotfiles/config/kitty /home/$USER/.config/. 
 
 #links pictures and other stuff
 ln -sf /home/$USER/Github/hyprland-dotfiles/Pictures /home/$USER/.
